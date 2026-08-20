f4f0b9db4034ac0c1c272424f3a993aee2419667
desree-builds <desreempeka@gmail.com>
2026-08-20 22:21:13 +0300

    Add skills app and update backend configuration

--- portfolio_backend/urls.py
+++ portfolio_backend/urls.py
@@ -21,4 +21,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
+    path('api/', include('skills.urls'))
+
 ]
+
+
+
+
